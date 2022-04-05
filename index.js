f2c30c9681718a54903c1968a6d9de6ec69dbce3
mruf11 <99149959+mruf11@users.noreply.github.com>
2022-04-05 15:06:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,13 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  
 
-  .get('/times', (req, res) => res.send(showTimes()))
-  
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
