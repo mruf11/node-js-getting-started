5c8670ba7642d6d7bcdf0420c1f4b2c9b49442a0
mruf11 <99149959+mruf11@users.noreply.github.com>
2022-04-04 19:36:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
